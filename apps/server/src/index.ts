74392917824270f3f2ca813d05f7713b0378f0dd
srjvshelke <116488745+srjvshelke@users.noreply.github.com>
2024-08-19 01:12:54 +0530

    up[

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,20 +1,26 @@
+import express from 'express';
 import http from "http";
+
+
+const app = express();
 // import SocketService from "./services/socket";
 // import { startMessageConsumer } from "./services/kafka";
-async function init() {
+// async function init() {
 //   startMessageConsumer();
 //   const socketService = new SocketService();
 
-  const httpServer = http.createServer();
-  const PORT = process.env.PORT ? process.env.PORT : 8000;
+const httpServer = http.createServer();
+const PORT = process.env.PORT ? process.env.PORT : 8000;
 
 //   socketService.io.attach(httpServer);
-
-  httpServer.listen(PORT, () =>
-    console.log(`HTTP Server started at PORT:${PORT}`)
-  );
+app.get("/", (req, res) => {
+  res.send("hello")
+})
+app.listen(PORT, () =>
+  console.log(`HTTP Server started at PORT:${PORT}`)
+);
 
 //   socketService.initListeners();
-}
+// }
 
-init();
+// init();
